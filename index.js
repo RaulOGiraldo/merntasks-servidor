f495fbd4115e7266f53bf22bf1314a59336d2178
Raul Oswaldo Giraldo <raul_oswaldo@hotmail.com>
2020-07-18 00:13:42 -0500

    SE corrigio el index

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 // Habilitar express.json
 app.use( express.json({ extended: true }));
 
-// puerto de la app
+// Puerto de la app
 const port = process.env.port || 4000;
 
 // Importar rutas
@@ -23,7 +23,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
-// arrancar la app
+// Arrancar la app
 app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
