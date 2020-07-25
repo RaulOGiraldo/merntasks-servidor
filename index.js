85e3b1fc7f621c68a62c440c68f1ec5f24d153aa
Raul Oswaldo Giraldo <raul_oswaldo@hotmail.com>
2020-07-25 00:41:48 -0500

    arreglar el exted

--- index.js
+++ index.js
@@ -12,21 +12,25 @@ conectarDB();
 app.use(cors());
 
 // Habilitar express.json
-app.use( express.json({ extended: true }));
+app.use(express.json({ extend: true }));
 
 // Puerto de la app
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 // Importar rutas
+// Usuarios
 app.use('/api/usuarios', require('./routes/usuarios'));
+// Autenticación
 app.use('/api/auth', require('./routes/auth'));
+// Proyectos
 app.use('/api/proyectos', require('./routes/proyectos'));
+// Tareas
 app.use('/api/tareas', require('./routes/tareas'));
 
 // Definir la pagina principal
-app.get('/', (req, res) => {
-    res.send('Hola Mundo');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hola Mundo');
+// });
 
 // Arrancar la app
 app.listen(port, '0.0.0.0', () => {
